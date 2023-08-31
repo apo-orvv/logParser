bd1928a878f7abee4f8e56e9522f83c58f9ee838
apo-orvv <apoorvdsp@gmail.com>
2023-08-31 06:31:47 +0530

    Add files via upload

---
+++ licenselog.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 30, 2023 at 10:42 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `licenselog`
+--
+
+DROP TABLE IF EXISTS `licenselog`;
+CREATE TABLE IF NOT EXISTS `licenselog` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Date` date DEFAULT NULL,
+  `Time` time DEFAULT NULL,
+  `Software` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+  `Feature` varchar(255) DEFAULT NULL,
+  `UserMachine` varchar(255) DEFAULT NULL,
+  `Licenses` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- model.php
+++ model.php
@@ -1,33 +1,66 @@
 <?php
 class LogModel {
-    public function processLog($logContent) {
-        $entryPattern = '/(\d{2}:\d{2}:\d{2}) \((.*?)\) (.*?)\: "(.*?)" (.*?) (\(\d+ licenses\))/';
-        preg_match_all($entryPattern, $logContent, $matches, PREG_SET_ORDER);
+    private $pdo;
 
+    public function __construct() {
+        // Update these parameters with your actual database credentials
+        $dsn = 'mysql:host=localhost;dbname=test;charset=utf8';
+        $username = 'root';
+        $password = '';
+
+        $this->pdo = new PDO($dsn, $username, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function processLog($logContent) {
         $data = [];
-        foreach ($matches as $entry) {
-            $timestampStr = $entry[1];
-            $software = $entry[2];
-            $status = $entry[3];
-            $feature = $entry[4];
-            $userMachine = $entry[5];
-            $licenses = $entry[6];
-            
-            $timestamp = DateTime::createFromFormat('H:i:s', $timestampStr);
-            $licensesCount = (int) preg_replace('/\D/', '', $licenses);
-            
-            $data[] = [
-                "Date" => $timestamp->format('Y-m-d'),
-                "Time" => $timestamp->format('H:i:s'),
-                "Software" => $software,
-                "Status" => $status,
-                "Feature" => $feature,
-                "User Machine" => $userMachine,
-                "Licenses" => $licensesCount
-            ];
+        $currentDate = null;
+
+        $lines = explode("\n", $logContent);
+
+        foreach ($lines as $line) {
+            if (preg_match('/(\d{2}:\d{2}:\d{2}) \((.*?)\) (.*?)\: "(.*?)" (.*?) (\(\d+ licenses\))/', $line, $matches)) {
+                $timestampStr = $matches[1];
+                $software = $matches[2];
+                $status = $matches[3];
+                $feature = $matches[4];
+                $userMachine = $matches[5];
+                $licensesCount = (int)preg_replace('/[^\d]/', '', $matches[6]);
+
+                $timestamp = DateTime::createFromFormat('H:i:s', $timestampStr);
+
+                if ($currentDate !== null) {
+                    $data[] = [
+                        "Date" => $currentDate,
+                        "Time" => $timestamp->format('H:i:s'),
+                        "Software" => $software,
+                        "Status" => $status,
+                        "Feature" => $feature,
+                        "User Machine" => $userMachine,
+                        "Licenses" => $licensesCount
+                    ];
+                }
+            } elseif (strpos($line, "TIMESTAMP") !== false) {
+                $dateParts = explode(" ", $line);
+                $currentDateStr = end($dateParts);
+                $currentDate = DateTime::createFromFormat('m/d/Y', $currentDateStr)->format('Y-m-d');
+            }
+
+            if ($currentDate !== null) {
+                // Insert data into the database
+                $this->insertDataToDatabase($currentDate, $timestamp->format('H:i:s'), $software, $status, $feature, $userMachine, $licensesCount);
+            }
         }
-        
+
         return $data;
     }
+
+    private function insertDataToDatabase($date, $time, $software, $status, $feature, $userMachine, $licenses) {
+        $sql = "INSERT INTO licenselog (Date, Time, Software, Status, Feature, UserMachine, Licenses) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$date, $time, $software, $status, $feature, $userMachine, $licenses]);
+    }
 }
+
+
 ?>
